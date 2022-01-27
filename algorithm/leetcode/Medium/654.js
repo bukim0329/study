0e3a6018522736b1d51939b80dd8a0dbad91924f
bukim0329 <bukim0329@gmail.com>
2022-01-27 23:05:44 +0900

    add: Add 1/27/2022 problem solving

---
+++ algorithm/leetcode/Medium/654.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function (nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+  const ind = nums.indexOf(Math.max(...nums));
+  return new TreeNode(
+    Math.max(...nums),
+    constructMaximumBinaryTree(nums.slice(0, ind)),
+    constructMaximumBinaryTree(nums.slice(ind + 1))
+  );
+};
