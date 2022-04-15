23249b1367484de49bb715b5157425dc9eba5282
bukim0329 <bukim0329@gmail.com>
2022-04-15 23:53:15 +0900

    add: Add 4/15/2022 problem solving

---
+++ algorithm/leetcode/Medium/1578.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCost = (s, cost) => {
+  let res = 0;
+  for (let i = 0, max = 0; i < s.length; i++) {
+    res += cost[i];
+    max = Math.max(max, cost[i]);
+    if (s[i] !== s[i + 1]) {
+      res -= max;
+      max = 0;
+    }
+  }
+  return res;
+};
