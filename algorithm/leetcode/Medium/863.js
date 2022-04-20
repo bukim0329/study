b881ca2dab2acf9e1df847dc070e86218d0abfd0
bukim0329 <bukim0329@gmail.com>
2022-04-20 23:51:02 +0900

    add: Add 4/20/2022 problem solving

---
+++ algorithm/leetcode/Medium/863.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+
+const distanceK = function (root, target, K) {
+  if (!root) return [];
+  const node = findTarget(root, null, target);
+  const res = [];
+  findAllKApart(node, K, res);
+  return res;
+};
+
+const findTarget = (root, parent, target) => {
+  if (!root) return null;
+  root.parent = parent;
+  if (root === target) {
+    return root;
+  }
+  return findTarget(root.left, root, target) || findTarget(root.right, root, target);
+};
+
+const findAllKApart = (root, k, res) => {
+  if (!root || root.visited) return res;
+  if (k === 0) {
+    res.push(root.val);
+    return res;
+  }
+  root.visited = true;
+  findAllKApart(root.left, k - 1, res);
+  findAllKApart(root.right, k - 1, res);
+  findAllKApart(root.parent, k - 1, res);
+  return res;
+};
