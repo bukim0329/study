c17325b6409f530be683ad54e4200d72af12fc29
bukim0329 <bukim0329@gmail.com>
2022-05-05 23:59:23 +0900

    add: Add 5/5/2022 problem solving

--- algorithm/leetcode/Easy/35.js
+++ algorithm/leetcode/Easy/35.js
@@ -4,20 +4,15 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-  let low = 0;
-  let high = nums.length - 1;
-  while (low <= high) {
-    const mid = Math.floor((low + high) / 2);
+  let min = 0;
+  let max = nums.length - 1;
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
     if (nums[mid] < target) {
-      low = mid + 1;
+      min = mid + 1;
     } else {
-      high = mid - 1;
+      max = mid - 1;
     }
   }
-  return low;
-};
-
-const searchInsert = function (nums, target) {
-  nums.push(target);
-  return nums.sort((a, b) => a - b).indexOf(target);
+  return min;
 };
