6d670aca3a1ed97f7c5f57a1b8d5f5a6606158a4
bukim0329 <bukim0329@gmail.com>
2022-05-21 08:21:00 +0900

    add: Add 5/21/2022 problem solving

---
+++ algorithm/leetcode/Medium/148.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const sortList = head => {
+  if (!head || !head.next) return head;
+  let fast = head;
+  let slow = head;
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  let middle = slow.next;
+  slow.next = null;
+  return merge(sortList(head), sortList(middle));
+};
+
+const merge = (o, t) => {
+  let d = new ListNode(-1);
+  let tmp = d;
+  while (o && t) {
+    tmp.next = o.val < t.val ? o : t;
+    tmp = tmp.next;
+    if (o.val < t.val) o = o.next;
+    else t = t.next;
+  }
+  if (o) tmp.next = o;
+  if (t) tmp.next = t;
+  return d.next;
+};
